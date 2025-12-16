575c02f3c3b514c8394d383d5c35c4afdadea00f
Sai Jayakar Vanam <103515219+Jayakar72@users.noreply.github.com>
2025-12-16 02:57:11 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `./src/images/GoosePhoto.jpg`, // This path is relative to the root of the site.
+        icon: `./src/images/goofyphoto.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
