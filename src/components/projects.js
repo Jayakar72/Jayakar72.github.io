b3aa4dbcdf0764f0fb39a51fb66c46a137905679
Sai Jayakar Vanam <103515219+Jayakar72@users.noreply.github.com>
2025-12-17 17:20:16 -0500

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,7 +5,7 @@ import { useLanguage } from "../contexts/LanguageContext"
 import data, { getText } from "../data"
 import "../styles/projects.scss"
 
-import trajpred from "../images/ProjectPhotos/demotrajpred.gif"
+import trajpred from "../images/ProjectPhotos/demotrajpred.mp4"
 import watoImage from "../images/ProjectPhotos/Wato.webp"
 import hisecurityImage from "../images/ProjectPhotos/HiSecurity2.webp"
 import pilotImg from "../images/ProjectPhotos/Pilot.webp"
