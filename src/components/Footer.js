85b27ef5b24101e859beeb3edd6657c9b13e56ac
Sai Jayakar Vanam <103515219+Jayakar72@users.noreply.github.com>
2025-12-19 15:40:42 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
 import React from "react"
 import Fade from "./animations/Fade"
-import { useLanguage } from "../contexts/LanguageContext"
 import data, { getText } from "../data"
 import "../styles/footer.scss"
 
 const Footer = () => {
-  const { language } = useLanguage();
 
   return (
     <div className="section" id="contact">
