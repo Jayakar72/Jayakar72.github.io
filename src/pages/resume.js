7a188a1a926029f0b2968293021e28c99b5b3435
Sai Jayakar Vanam <103515219+Jayakar72@users.noreply.github.com>
2026-05-29 22:46:47 -0400

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -12,7 +12,7 @@ const ResumeContent = () => {
   
   // Resume PDF URLs - can be configured for different languages
   const resumeUrls = {
-    en: "https://drive.google.com/file/d/1NdrNamusOwL2vRp7GhXDDh-LZSEtO0uR/preview",
+    en: "https://drive.google.com/file/d/16EGvf_AvTouKNJ-BQ-U4HDSesKzLF9Q8/preview",
   };
   
   const resumeUrl = resumeUrls[language] || resumeUrls.en;
